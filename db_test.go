4d3cd76b0dfb4201d010830ac38a7cde7fd15b31
Alex Zylman <alex.zylman@clever.com>
2016-03-18 00:34:33 +0000

    add tests

--- db_test.go
+++ db_test.go
@@ -1,10 +1,34 @@
 package main
 
-import "testing"
+import (
+	"log"
+	"testing"
 
-func TestNewDB(t *testing.T) {
-	_, err := NewGeoDB(*path)
+	"github.com/stretchr/testify/assert"
+)
+
+var db *GeoDB
+
+func init() {
+	// Just to make benchmarks faster
+	var err error
+	db, err = NewGeoDB(*path)
 	if err != nil {
-		t.Fatal(err)
+		log.Fatal(err)
+	}
+}
+
+func TestDB(t *testing.T) {
+	assert := assert.New(t)
+
+	latlon, err := db.Lookup("216.75.229.114")
+	assert.Nil(err)
+
+	assert.Equal(latlon, &LatLon{37.555, -122.2687})
+}
+
+func BenchmarkLookup(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		db.Lookup("216.75.229.114")
 	}
 }
